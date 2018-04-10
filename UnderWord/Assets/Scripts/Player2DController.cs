66477153e46c394bf9bf3603e54893e19a5c0c2d
Gvnnbleid <krawczykmf@gmail.com>
2018-04-10 23:34:54 +0200

    Add joystick

---
+++ UnderWord/Assets/Scripts/Player2DController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player2DController : MonoBehaviour
+{
+    public float moveForce = 5;
+    Rigidbody2D myBody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Vector2 myVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"),
+            CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
+        myBody.AddForce(myVec);
+	}
+}
