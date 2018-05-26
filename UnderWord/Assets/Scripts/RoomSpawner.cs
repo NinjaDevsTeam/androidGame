1af0779df9e8bee5a762c96e89ba5f5d4c689f19
Maciej Krawczyk <krawczykmf@gmail.com>
2018-05-26 23:58:07 +0200

    Player starts game in a procedural generated empty dungeon

--- UnderWord/Assets/Scripts/RoomSpawner.cs
+++ UnderWord/Assets/Scripts/RoomSpawner.cs
@@ -25,22 +25,22 @@ public class RoomSpawner : MonoBehaviour
 		if(openingDirection == OpeningDirection.BottomDoor)
         {
             rand = Random.Range(0, templates.bottomRooms.Length);
-            Instantiate(templates.bottomRooms[rand], new Vector3(transform.position.x+7, transform.position.y+2), templates.bottomRooms[rand].transform.rotation);
+            Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
         }
         else if(openingDirection == OpeningDirection.TopDoor)
         {
             rand = Random.Range(0, templates.topRooms.Length);
-            Instantiate(templates.topRooms[rand], new Vector3(transform.position.x + 7, transform.position.y + 2), templates.topRooms[rand].transform.rotation);
+            Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
         }
         else if(openingDirection == OpeningDirection.LeftDoor)
         {
             rand = Random.Range(0, templates.leftRooms.Length);
-            Instantiate(templates.leftRooms[rand], new Vector3(transform.position.x + 7, transform.position.y + 2), templates.leftRooms[rand].transform.rotation);
+            Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
         }
         else if(openingDirection == OpeningDirection.RightDoor)
         {
             rand = Random.Range(0, templates.rightRooms.Length);
-            Instantiate(templates.rightRooms[rand], new Vector3(transform.position.x + 7, transform.position.y + 2), templates.rightRooms[rand].transform.rotation);
+            Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
         }
         spawned = true;
 	}
@@ -49,13 +49,15 @@ public class RoomSpawner : MonoBehaviour
     {
         if(other.CompareTag("SpawnPoint"))
         {
-            if(!other.GetComponent<RoomSpawner>().spawned && !spawned)
+            if (!other.GetComponent<RoomSpawner>().spawned && !spawned)
             {
                 Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
                 Destroy(gameObject);
             }
             spawned = true;
         }
+        if (other.CompareTag("Destroyer"))
+            Destroy(gameObject);
     }
 }
 
